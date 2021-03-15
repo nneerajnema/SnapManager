96c660c35176b44b1cec0a2370094e9bc93bf01a
Neeraj Nema <nemaneeraj@yahoo.com>
2021-03-15 21:16:32 +0530

    Adding Main file

--- src/com/main/raw/RAWDelete.java
+++ src/com/main/raw/RAWDelete.java
@@ -6,6 +6,26 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/*
+A Java8 based project to delete irrelevant RAW files after a shoot when corresponding JPEG files have been deleted.
+
+The code needs three variables to start with:
+baseDir: Base directory containing all event specific folders
+eventFolder: Directory inside <baseDir> of the specific event, e.g., Birthday Celebration. The directory houses all JPEG files imported from camera memory.
+rawFolder: Directory which houses all RAW files. It may be RAW / NRW / CR2/ etc.
+
+Windows example
+C:\
+└───Pics
+    └───Birthday 2021
+        └───RAW
+
+Based on the above directory structure, the value of variables will be:
+--> baseDir: C:/Pics/
+--> eventFolder: Birthday 2021
+--> rawFolder: RAW
+
+*/
 public class RAWDelete {
 
     /**
@@ -14,12 +34,13 @@ public class RAWDelete {
     public static void main(String[] args) {
 
         /* Base dir of all pics */
-        String baseDir = "C:\\Neeraj\\Pics\\";
+        String baseDir = "";
 
-        /* Pic Dir */
-        String eventFolder = "Ghughra";
+        /* Pic dir */
+        String eventFolder = "";
 
-        String rawFolder = "RAW";
+        /* RAW folder name */
+        String rawFolder = "";
 
         /* Directory containing JPEG files */
         String jpegFilesPath = baseDir + eventFolder;
